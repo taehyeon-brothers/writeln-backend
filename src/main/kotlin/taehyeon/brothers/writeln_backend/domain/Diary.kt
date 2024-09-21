ed302751e646101a2e6a7bc534135c4b47bf8c61
김태현 <kth990303@woowahan.com>
2024-09-21 17:41:35 +0900

    feat: diary 클래스 구현

---
+++ src/main/kotlin/taehyeon/brothers/writeln_backend/domain/Diary.kt
@@ -0,0 +1,21 @@
+package taehyeon.brothers.writeln_backend.domain
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "diaries")
+class Diary(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "user_id", nullable = false)
+    var userId: Long,
+
+    @Column(name = "title", length = 100, nullable = false)
+    var title: String,
+
+    @Column(name = "content", length = 4000)
+    var content: String
+)
